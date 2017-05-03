c96842f08d84f232bc1f5e3a1b95c3584cd1fc29
Steven Shaw <steven@steshaw.org>
2017-05-03 21:49:30 +1000

    A couple of small Yesod examples from yesod-static

---
+++ static.hs
@@ -0,0 +1,27 @@
+-- From https://github.com/yesodweb/yesod/blob/master/yesod-static/sample.hs
+
+{-# LANGUAGE QuasiQuotes, TypeFamilies, MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+import Yesod.Static
+import Yesod.Core
+import Network.Wai.Handler.Warp (run)
+import Network.Wai.Application.Static
+
+staticFiles "."
+
+data Sample = Sample { getStatic :: Static }
+
+--getStatic _ = Static $ defaultFileServerSettings { ssFolder = fileSystemLookup $ toFilePath "." }
+mkYesod "Sample" [parseRoutes|
+/ RootR GET
+/static StaticR Static getStatic
+|]
+instance Yesod Sample where
+
+getRootR = do
+    redirect "static"
+    return ()
+
+main = do
+    s <- static "."
+    toWaiApp (Sample s) >>= run 3000
