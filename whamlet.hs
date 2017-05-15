ffa80cd33e79b1f51c47050998995aa2565360a8
Steven Shaw <steven@steshaw.org>
2017-05-15 14:09:02 +1000

    whamlet example

---
+++ whamlet.hs
@@ -0,0 +1,51 @@
+#!/usr/bin/env stack
+{-
+  stack script
+    --resolver lts-8.8
+    --package yesod
+    --
+    -Wall -fwarn-tabs
+-}
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Yesod
+
+data App = App
+
+mkYesod "App" [parseRoutes|
+  / HomeR GET
+|]
+
+instance Yesod App where
+
+page :: WidgetT App IO ()
+page =
+  [whamlet|
+    <p>This is my page. I hope you enjoyed it.
+    ^{footer}
+  |]
+
+footer :: WidgetT App IO ()
+footer = do
+  toWidget
+    [lucius|
+      footer {
+        font-weight: bold;
+        text-align: center
+      }
+    |]
+  toWidget
+    [hamlet|
+      <footer>
+        <p>That's all folks!
+    |]
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout page
+
+main :: IO ()
+main = warp 3000 App
